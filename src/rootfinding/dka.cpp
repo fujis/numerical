e4bd47c74ee9eb422544dbe10e197de09174e30d
fujis <fujisw@gmail.com>
2019-10-17 21:09:20 +0900

    fix: dka-aberth

--- src/rootfinding/dka.cpp
+++ src/rootfinding/dka.cpp
@@ -19,6 +19,16 @@
 typedef complex<double> complexf;	// double型の複素数を定義
 
 
+//! complex型に<<オペレータを設定(演算子のオーバーロード)
+template<typename T>
+inline std::ostream &operator<<(std::ostream &out, const complex<T> &x)
+{
+	out << x.real();
+	if(fabs(x.imag()) > 1e-6) out << " + " << x.imag() << "i";
+	return out;
+}
+
+
 //-----------------------------------------------------------------------------
 // 代数方程式
 //-----------------------------------------------------------------------------
@@ -46,10 +56,9 @@ inline T func(T x, const vector<T> &b, int n)
  * @param[in] n 方程式の次数
  * @return 代数方程式の値
  */
-template<class T>
-inline T func_h(double x, const vector<T> &b, int n)
+inline double func_h(double x, const vector<double> &b, int n)
 {
-	T f = b[0];
+	double f = b[0];
 	for(int i = 1; i <= n; ++i){
 		f = b[i]+f*x;
 	}
@@ -62,10 +71,9 @@ inline T func_h(double x, const vector<T> &b, int n)
  * @param[in] n 方程式の次数
  * @return 代数方程式の導関数値
  */
-template<class T>
-inline T dfunc_h(double x, const vector<T> &b, int n)
+inline double dfunc_h(double x, const vector<double> &b, int n)
 {
-	T df = n*b[0];
+	double df = n*b[0];
 	for(int i = 1; i <= n-1; ++i){
 		df = (n-i)*b[i]+df*x;
 	}
@@ -114,22 +122,25 @@ int newton(const vector<double> &b, int n, double &x, int &max_iter, double &eps
 	return 0;
 }
 
-/*!
- * ホーナー法(組立除法)の係数の値を求める
- * @param[in] a 代数方程式の係数
- * @param[out] b x=x0のときの組立除法の係数
- * @param[in] x0 係数を計算するxの値
- */
-template<class T>
-inline void horner(const vector<T> &a, vector<T> &b, int n, T x0)
+ /*!
+  * ホーナー法(組立除法)
+   - P(x) = a0 x^n + a1 x^(n-1) + ... + a_(n-1) x + a_n を (x-b) で割ったときの商と余りを返す
+   - 商はn-1次の多項式の係数として返す
+  * @param[in] a 代数方程式の係数
+  * @param[in] b 割る1次式の係数(x-b)
+  * @param[out] c 商であるn-1次の多項式の係数
+  * @param[out] rm あまり
+  * @param[in] n 方程式の次数(配列aの大きさはn+1,配列cはn)
+  */
+inline void horner(const vector<double> &a, double b, vector<double> &c, double &rm, int n)
 {
-	if(n <= 2) return;
-
-	b = a;
-	for(int i = 0; i <= n; ++i){
-		for(int j = 1; j <= n-i; ++j){
-			b[j] += x0*b[j-1];
-		}
+	if(n <= 1) return;
+	rm = a[0]; // 最終的に余りになる
+	c.resize(n);
+	for(int i = 1; i < n+1; ++i){
+		c[i-1] = rm;
+		rm *= b;
+		rm += a[i];
 	}
 }
 
@@ -145,26 +156,50 @@ inline void horner(const vector<T> &a, vector<T> &b, int n, T x0)
 int aberth(vector<complexf> &z, const vector<complexf> &c, int n, int max_iter, double eps)
 {
 	// 半径算出のための方程式の係数
-	vector<complexf> a;
-	complexf zc = -c[1]/(c[0]*(double)n);
-	horner(c, a, n, zc);
+	vector<complexf> cd(n+1, 1.0); // 係数c'
+	double c1n = -c[1].real()/n; 
+	cd[0] = c[0];
+	double rm; // 組立除法での余り
+	vector<double> a(n+1), tmp(n); // 係数格納用の一時的な変数
+	for(int i = 0; i <= n; ++i) a[i] = c[i].real();
+	// zの多項式をz+c1/nで割っていくことでwの多項式の係数を求める
+	for(int i = n; i > 1; --i){
+		horner(a, c1n, tmp, rm, i);
+		cd[i] = rm;
+		a = tmp;
+	}
+	cd[1] = a[1]+c1n;
 
-	vector<double> b(a.size());
-	b[0] = abs(a[0]);
+	// 多項式S(w)の係数
+	vector<double> b(cd.size());
+	b[0] = abs(cd[0]);
 	for(int i = 1; i <= n; ++i){
-		b[i] = -abs(a[i]);
+		b[i] = -abs(cd[i]);
 	}
 
 	// Aberthの初期値の半径をニュートン法で算出
-	double r = 100.0;
+	int m = 0; // 係数cの中で0でないものの個数
+	for(int i = 0; i <= n; ++i) m += (fabs(c[i].real()) > 1e-6 ? 1 : 0);
+	double rmax = 0.0; // 半径の最大値
+	for(int i = 1; i <= n; ++i){
+		double ri = pow(m*fabs(c[i].real())/fabs(c[0].real()), 1.0/(i+1.0));
+		if(ri > rmax) rmax = ri;
+	}
+	cout << "r_max = " << rmax << endl;
+	double r = rmax;
 	newton(b, n, r, max_iter, eps);
-	//cout << "r = " << r << endl;
+	cout << "r = " << r << endl;
+	r = 10;
 
 	// Aberthの初期値
+	complexf zc = -c[1]/(c[0]*(double)n);
 	for(int j = 0; j < n; ++j){
 		double theta = (2*RX_PI/(double)n)*j+RX_PI/(2.0*n);
 		z[j] = zc+r*complexf(cos(theta), sin(theta));
+		cout << "x" << j << "(0) = " << z[j] << (j == n-1 ? "" : ", ");
 	}
+	cout << endl;
+	
 
 	return 1;
 }
@@ -181,6 +216,10 @@ int aberth(vector<complexf> &z, const vector<complexf> &c, int n, int max_iter, 
 int weierstrass(vector<complexf> &z, const vector<complexf> &c, int n, int &max_iter, double &eps)
 {
 	double e = 0.0, ej;
+
+	cout << "0, ";
+	for(int i = 0; i < n; ++i) cout << z[i].real() << ", " << z[i].imag() << ", ";
+	cout << endl;
 
 	vector<complexf> zp;
 	complexf f, df;
@@ -200,6 +239,10 @@ int weierstrass(vector<complexf> &z, const vector<complexf> &c, int n, int &max_
 
 			z[j] = zp[j]-f/df;
 		}
+
+		cout << k+1 << ", ";
+		for(int i = 0; i < n; ++i) cout << z[i].real() << ", " << z[i].imag() << (i == n-1 ? "" : ", ");
+		cout << endl;
 
 		// 誤差の算出
 		e = 0.0;
@@ -231,17 +274,32 @@ int main(void)
 	vector<complexf> c;
 	vector<double> c0;
 	vector<complexf>::iterator itr;
+	int n;	// 代数方程式の次数
 
 	// 代数方程式の係数を読み込む
 	if(ReadAlgebra("algebra.txt", ",", c0)){
-		for(vector<double>::iterator i = c0.begin(); i != c0.end(); ++i){
-			c.push_back(complex<double>(*i));
+		n = (int)c0.size()-1;
+		c.resize(n+1);
+		for(int i = 0; i <= n; ++i){
+			c[i] = complex<double>(c0[i]);
 		}
 	}
+	else{
+		cout << "file read error!" << endl;
+		return 1;
+	}
 
-	int n = (int)c.size()-1;		// 代数方程式の次数
+	// (確認用)元の方程式の表示
+	cout << "f(x) = ";
+	for(int i = 0; i < n; ++i){
+		cout << c0[i] << "x^" << n-i << " + ";
+	}
+	cout << c0[n] << " = 0" << endl;
+	cout << endl;
+
+
 	int max_iter = 100;
-	double eps = 1e-8;
+	double eps = 1e-6;
 
 	// Aberthの初期値
 	vector<complexf> z(n);
@@ -252,8 +310,17 @@ int main(void)
 
 	// 結果の画面表示
 	cout << "solutions : " << endl;
-	for(itr = z.begin(); itr != z.end(); ++itr){
-		cout << "  " << *itr << endl;
+	for(int i = 0; i < n; ++i){
+		cout << "x" << i << " = " << z[i];
+
+		// 解の精度のチェック(f(x)を計算してみる)
+		complexf f = c0[n], zi = z[i];
+		for(int j = n-1; j >= 0; --j){
+			f += c0[j]*zi; zi *= z[i];
+		}
+		cout << "  --> f = " << f;
+		if(sqrt(f.real()*f.real()+f.imag()*f.imag()) < 1e-6) cout << " (OK)";
+		cout << endl;
 	}
 	cout << endl;
 	cout << "iter = " << max_iter << ", eps = " << eps << endl;
--- src/rootfinding/newton-raphson.cpp
+++ src/rootfinding/newton-raphson.cpp
@@ -219,4 +219,3 @@ int main(void)
 	return 0;
 }
 
-
